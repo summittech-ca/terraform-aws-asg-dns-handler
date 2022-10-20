f128c079d0f64692fb7304fff5bd538673f8ba8d
Eric Bellotti <ericb@summit-tech.ca>
2022-10-20 04:39:26 +0000

    Cleanup unnecessary output

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,6 @@ output "autoscale_handling_sns_topic_arn" {
   value       = aws_sns_topic.autoscale_handling.arn
 }
 
-output "autoscale_iam_role_arn" {
-  description = "IAM role ARN for autoscaling group"
-  value       = aws_iam_role.autoscale_handling.arn
-}
-
 output "agent_lifecycle_iam_role_arn" {
   description = "IAM Role ARN for lifecycle_hooks"
   value       = aws_iam_role.lifecycle.arn
